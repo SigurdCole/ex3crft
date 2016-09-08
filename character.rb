9e94795265051d92f29869159e013c51d4353939
SigurdCole <sigurd.cole@gmail.com>
2016-09-08 12:51:26 -0800

    motes are now calculated from essence
    also added output log

--- character.rb
+++ character.rb
@@ -1,6 +1,6 @@
 class Character
   
-  attr_accessor :essence, :intelligence, :craft, :sxp, :gxp, :wxp, :motes, :wp, :hl0, :hl1, :hl2, :hl4
+  attr_accessor :essence, :intelligence, :craft, :sxp, :gxp, :wxp, :motes1, :motes2, :wp, :hl0, :hl1, :hl2, :hl4
   
   def initialize(charms = Hash.new, stats = Hash.new)
     charms.is_a?(Hash) ? @charms = charms : @charms = Hash.new
@@ -11,11 +11,12 @@ class Character
     @sxp = stats[:sxp] ||= 1
     @gxp = stats[:gxp] ||= 1
     @wxp = stats[:wxp] ||= 1
-    @motes = stats[:motes] ||= 1
+    @motes1 = @essence * 3 + 10
+    @motes2 = @essence * 7 + 26
     @wp = stats[:wp] ||= 1
     @hl0 = stats[:hl0] ||= 1
-    @hl1 = stats[:hl1] ||= 1
-    @hl2 = stats[:hl2] ||= 1
+    @hl1 = stats[:hl1] ||= 2
+    @hl2 = stats[:hl2] ||= 2
     @hl4 = stats[:hl4] ||= 1
   end
 
--- test_character.rb
+++ test_character.rb
@@ -2,12 +2,12 @@ require "./character.rb"
 
 charms = {}
 
-stats = {:essence => 3, :intelligence => 4, :craft => 5, :sxp => 11, :gxp => 12, :wxp => 13, :motes => 14, :wp => 6, :hl0 => 1, :hl1 => 2, :hl2 => 2, :hl4 => 1}
+stats = {:essence => 3, :intelligence => 4, :craft => 5, :sxp => 11, :gxp => 12, :wxp => 13, :wp => 6, :hl0 => 1, :hl1 => 2, :hl2 => 2, :hl4 => 1}
 
 puts "Input Stats"
 puts "Essence #{stats[:essence]}, Intelligence #{stats[:intelligence]}, Craft #{stats[:craft]}"
 puts "Silver XP #{stats[:sxp]}, Gold XP #{stats[:gxp]}, White XP #{stats[:wxp]}"
-puts "Motes #{stats[:motes]}, Willpower #{stats[:wp]}"
+puts "Willpower #{stats[:wp]}"
 puts "Health Levels: #{stats[:hl0]} -0, #{stats[:hl1]} -1, #{stats[:hl2]} -2, #{stats[:hl4]} -4,"
 puts
 
@@ -16,5 +16,5 @@ test = Character.new(charms,stats)
 puts "Output Stats"
 puts "Essence #{test.essence}, Intelligence #{test.intelligence}, Craft #{test.craft}"
 puts "Silver XP #{test.sxp}, Gold XP #{test.gxp}, White XP #{test.wxp}"
-puts "Motes #{test.motes}, Willpower #{test.wp}"
+puts "Personal Motes #{test.motes1}, Peripheral Motes #{test.motes2}, Willpower #{test.wp}"
 puts "Health Levels: #{test.hl0} -0, #{test.hl1} -1, #{test.hl2} -2, #{test.hl4} -4,"
